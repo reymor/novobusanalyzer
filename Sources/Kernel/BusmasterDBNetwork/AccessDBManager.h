7406b828bf6ae8822372adf5652795a0bb484f35
Reyders Morales <reyders1@gmail.com>
2024-01-21 02:05:52 +0100

    KernelDBNetwork: Add include guard instead of pragma once. Format using google code style

--- Sources/Kernel/BusmasterDBNetwork/AccessDBManager.h
+++ Sources/Kernel/BusmasterDBNetwork/AccessDBManager.h
@@ -1,23 +1,30 @@
-#pragma once
+#ifndef KERNEL_BUSMASTERDBNETWORK_ACCESSDBMANAGER_H_
+#define KERNEL_BUSMASTERDBNETWORK_ACCESSDBMANAGER_H_
+
 #include <Windows.h>
-#include <string>
+
 #include <list>
 #include <string>
+
 #include "ICluster.h"
 
-class AccessDBManager
-{
-    typedef ERRORCODE( *PARSEDBFILE )( std::string strFileName, ETYPE_BUS clusterType, std::list<ClusterResult>& ouClusterResultList );
-    typedef ERRORCODE( *FREECLUSTER )( ICluster* );
-    HMODULE mDllHandle;
-    bool mDbManagerAvailable;
-public:
-    AccessDBManager();
-    PARSEDBFILE mParseDbFile;
-    FREECLUSTER mFreeCluster;
-    bool isDbManagerAvailable();
-    HRESULT LoadDbManager();
-    void ReleaseDbmanager();
-    ~AccessDBManager();
+
+class AccessDBManager {
+  typedef ERRORCODE (*PARSEDBFILE)(
+      std::string strFileName, ETYPE_BUS clusterType,
+      std::list<ClusterResult>& ouClusterResultList);
+  typedef ERRORCODE (*FREECLUSTER)(ICluster*);
+  HMODULE mDllHandle;
+  bool mDbManagerAvailable;
+
+ public:
+  AccessDBManager();
+  PARSEDBFILE mParseDbFile;
+  FREECLUSTER mFreeCluster;
+  bool isDbManagerAvailable();
+  HRESULT LoadDbManager();
+  void ReleaseDbmanager();
+  ~AccessDBManager();
 };
 
+#endif  // KERNEL_BUSMASTERDBNETWORK_ACCESSDBMANAGER_H_
--- Sources/Kernel/BusmasterDBNetwork/Include/IBMNetWorkService.h
+++ Sources/Kernel/BusmasterDBNetwork/Include/IBMNetWorkService.h
@@ -1,18 +1,29 @@
-#pragma once
-#include "ICluster.h"
+#ifndef KERNEL_BUSMASTERDBNETWORK_IBMNETWORKSERVICE_H_
+#define KERNEL_BUSMASTERDBNETWORK_IBMNETWORKSERVICE_H_
+
 #include "IBMNetWorkGetService.h"
-class IBMNetWorkService : public IBMNetWorkGetService
-{
-public:
-    virtual ~IBMNetWorkService() {};
-    virtual ERRORCODE SetChannelSettings( ETYPE_BUS eouProtocol, int nChannelIndex, ChannelSettings* ) = 0;
-    virtual ERRORCODE SetDBService( ETYPE_BUS eouProtocol, int nChannelIndex, int nDBIndex, ICluster* ) = 0;
-    virtual ERRORCODE DeleteDBService( ETYPE_BUS eouProtocol, int nChannelIndex, std::string dbPath ) = 0;
-    virtual ERRORCODE AddDBService( ETYPE_BUS eouProtocol, int nChannelIndex, ICluster* ) = 0;
-    virtual ERRORCODE LoadDb( ETYPE_BUS eouProtocol, int nChannelIndex, std::string ) = 0;
-    virtual ERRORCODE SetChannelCount( ETYPE_BUS eouProtocol, int nCount ) = 0;
-    virtual ERRORCODE ParseDbFile( std::string strFileName, ETYPE_BUS clusterType, std::list<ICluster*>& ) = 0;
-    virtual ERRORCODE SetSimulatedEcuList( ETYPE_BUS, int nChannelIndex, std::list<IEcu*>& ) = 0;
-	virtual ERRORCODE ReSetNetwork(ETYPE_BUS eouProtocol) = 0;
-    virtual bool isDbManagerAvailable() = 0;
+#include "ICluster.h"
+
+class IBMNetWorkService : public IBMNetWorkGetService {
+ public:
+  virtual ~IBMNetWorkService(){};
+  virtual ERRORCODE SetChannelSettings(ETYPE_BUS eouProtocol, int nChannelIndex,
+                                       ChannelSettings*) = 0;
+  virtual ERRORCODE SetDBService(ETYPE_BUS eouProtocol, int nChannelIndex,
+                                 int nDBIndex, ICluster*) = 0;
+  virtual ERRORCODE DeleteDBService(ETYPE_BUS eouProtocol, int nChannelIndex,
+                                    std::string dbPath) = 0;
+  virtual ERRORCODE AddDBService(ETYPE_BUS eouProtocol, int nChannelIndex,
+                                 ICluster*) = 0;
+  virtual ERRORCODE LoadDb(ETYPE_BUS eouProtocol, int nChannelIndex,
+                           std::string) = 0;
+  virtual ERRORCODE SetChannelCount(ETYPE_BUS eouProtocol, int nCount) = 0;
+  virtual ERRORCODE ParseDbFile(std::string strFileName, ETYPE_BUS clusterType,
+                                std::list<ICluster*>&) = 0;
+  virtual ERRORCODE SetSimulatedEcuList(ETYPE_BUS, int nChannelIndex,
+                                        std::list<IEcu*>&) = 0;
+  virtual ERRORCODE ReSetNetwork(ETYPE_BUS eouProtocol) = 0;
+  virtual bool isDbManagerAvailable() = 0;
 };
+
+#endif  // KERNEL_BUSMASTERDBNETWORK_IBMNETWORKSERVICE_H_
